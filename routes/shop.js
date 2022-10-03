f928c519d48d6d95f5207253acba01de2feb860f
Jyoti379 <haniyajo45@gmail.com>
2022-10-03 13:00:37 +0530

    message section of chat app

--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,32 @@
 const express = require('express');
-
+const fs= require('fs');
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  res.send('<h1>Hello from Express!</h1>');
+
+router.get('/', (req, res) => {
+  fs.readFile('username.txt',(err,data)=>{
+    if(err){
+      console.log(err)
+      data='No Data Exists'
+    }
+    res.send(`${data}<form action="/" onsubmit="document.getElementById('username').value=localStorage.getItem('username')"  method="POST">
+    <input type="text" id="message" name="message" placeholder="message">
+    <input type="hidden" name="username" id="username">
+    <button type="submit">send</button></form>`);
+  })
+
+ 
 });
+router.post('/', (req, res) => {
+  console.log(req.body.username)
+  console.log(req.body.message)
+  fs.writeFile("username.txt",`${req.body.username}:${req.body.message}`,{flag:'a'},(err)=>
+  err? console.log(err):res.redirect('/')
+  )
+ }
+ 
+ );
+
+
 
 module.exports = router;
