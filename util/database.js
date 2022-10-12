fe65d0657321c0cc0de5b6be9c528c5cd5548fb1
Jyoti379 <haniyajo45@gmail.com>
2022-10-12 16:34:58 +0530

    inserting in mysql

--- util/database.js
+++ util/database.js
@@ -1,9 +1,10 @@
-const mysql=require('mysql2');
-const pool =mysql.createPool({
-    host:'localhost',
-    user:'root',
-    database:'node-complete',
-    password:'JyotiSQL876'
-})
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'node-complete',
+    password: 'nodecomplete'
+});
 
 module.exports = pool.promise();
